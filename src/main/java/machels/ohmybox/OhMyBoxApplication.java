42032423917bf8f338f6197070eba8f767955d6c
Sondho <thseogh135@gmail.com>
2023-03-14 20:18:58 +0900

    chore: 프로젝트 세팅

---
+++ src/main/java/machels/ohmybox/OhMyBoxApplication.java
@@ -0,0 +1,13 @@
+package machels.ohmybox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OhMyBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OhMyBoxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/machels/ohmybox/OhMyBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package machels.ohmybox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OhMyBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
